5376830510a5b57ea82946348e46f347a23321e6
Shahzeb Khan <shahzeb8285@gmail.com>
2021-09-19 17:51:49 +0530

    utils added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const matic = require("./addresses/matic")
 const bsc = require("./addresses/bsc")
+const utils = require("./addresses/utils")
 
 export {matic as MATIC_TOKENS}
 
 export {bsc as BSC_TOKENS}
 
+export {utils as UTILS}
