273af62e5b6bf99d527165d2ca1a29e10aededb1
Shahzeb Khan <shahzeb8285@gmail.com>
2021-09-20 11:13:20 +0530

    lib build

--- index.js
+++
@@ -1,9 +0,0 @@
-const matic = require("./addresses/matic")
-const bsc = require("./addresses/bsc")
-const utils = require("./addresses/utils")
-
-export {matic as MATIC_TOKENS}
-
-export {bsc as BSC_TOKENS}
-
-export {utils as UTILS}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const matic = require("./addresses/matic")
+const bsc = require("./addresses/bsc")
+const utils = require("./addresses/utils")
+
+
+export {utils as UTILS,bsc as BSC_TOKENS,matic as MATIC_TOKENS}
